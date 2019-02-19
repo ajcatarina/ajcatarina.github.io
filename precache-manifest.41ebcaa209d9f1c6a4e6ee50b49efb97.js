f76d539b9dd0211d4cfc51d663c6d92207363bca
AJ <ajcatarina@outlook.com>
2019-02-20 01:18:03 +0800

    Add experimentation to publish build files

---
+++ precache-manifest.41ebcaa209d9f1c6a4e6ee50b49efb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5683a61a535b5467fb05",
+    "url": "/static/js/main.5683a61a.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5683a61a535b5467fb05",
+    "url": "/static/css/main.45cacdcc.chunk.css"
+  },
+  {
+    "revision": "247e915f63adbd7afadc5f303da5ae31",
+    "url": "/index.html"
+  }
+];
